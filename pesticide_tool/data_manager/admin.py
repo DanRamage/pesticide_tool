d4ab3200f975877a8883080ebef3c3d9277912de
dramage <danramage@Dans-MacBook-Pro.local>
2014-06-12 15:27:44 -0400

    checkin

--- pesticide_tool/data_manager/admin.py
+++ pesticide_tool/data_manager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
-class ActiveIngredient(admin.ModelAdmin):
+class ActiveIngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'row_id')
     search_fields = ['name']
     ordering = ('name')
