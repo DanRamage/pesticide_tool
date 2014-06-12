3b821427d4859ecfc1f4051923c71f5defb7aba9
dramage <danramage@Dans-MacBook-Pro.local>
2014-06-12 14:28:33 -0400

    model change

--- pesticide_tool/data_manager/admin.py
+++ pesticide_tool/data_manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+class ActiveIngredient(admin.ModelAdmin):
+    list_display = ('name', 'row_id')
+    search_fields = ['name']
+    ordering = ('name')
