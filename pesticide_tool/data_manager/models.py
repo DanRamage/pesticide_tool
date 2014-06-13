781e74e2ed4af4e2cefa237a12d4652560303948
dramage <danramage@Dans-MacBook-Pro.local>
2014-06-13 08:17:12 -0400

    ADmin config

--- pesticide_tool/data_manager/models.py
+++ pesticide_tool/data_manager/models.py
@@ -11,163 +11,175 @@
 from django.db import models
 
 class ActiveIngredient(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
-    cumulative_score = models.FloatField(blank=True, null=True)
-    relative_potential_ecosystem_hazard = models.CharField(max_length=50, blank=True)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
+  cumulative_score = models.FloatField(blank=True, null=True)
+  relative_potential_ecosystem_hazard = models.CharField(max_length=50, blank=True)
 
-    warnings = models.ManyToManyField('Warning', blank=True, null=True)
-    pests_treated = models.ManyToManyField('Pest')
-    pesticide_classes = models.ManyToManyField('PesticideClass')
-    brands = models.ManyToManyField('Brand')
+  warnings = models.ManyToManyField('Warning', blank=True, null=True)
+  pests_treated = models.ManyToManyField('Pest')
+  pesticide_classes = models.ManyToManyField('PesticideClass')
+  brands = models.ManyToManyField('Brand')
+
+  def __unicode__(self):
+    return self.name
 
 class PesticideClass(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
+
+  def __unicode__(self):
+    return self.name
 
 class Brand(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.TextField()
-    label_url = models.TextField(blank=True)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.TextField()
+  label_url = models.TextField(blank=True)
 
-    active_ingredients = models.ManyToManyField('BrandFormulation')
-    application_areas = models.ManyToManyField('ApplicationArea')
-    pests_treated = models.ManyToManyField('Pest')
+  active_ingredients = models.ManyToManyField('BrandFormulation')
+  application_areas = models.ManyToManyField('ApplicationArea')
+  pests_treated = models.ManyToManyField('Pest')
+
+  def __unicode__(self):
+    return self.name
 
 class BrandFormulation(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    brand_id = models.ForeignKey('Brand', null=False)
-    active_ingredient = models.ForeignKey('ActiveIngredient', null=False)
-    percentage_active_ingredient = models.FloatField(null=False)
-    
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  brand_id = models.ForeignKey('Brand', null=False)
+  active_ingredient = models.ForeignKey('ActiveIngredient', null=False)
+  percentage_active_ingredient = models.FloatField(null=False)
+
+  def __unicode__(self):
+    return self.name
+
 class Pest(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
-    image_url = models.TextField(blank=True)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
+  image_url = models.TextField(blank=True)
 
-    pesticides = models.ManyToManyField('ActiveIngredient', blank=True, null=True)
+  pesticides = models.ManyToManyField('ActiveIngredient', blank=True, null=True)
 
-   def __unicode__(self):
-       return self.name
+  def __unicode__(self):
+    return self.name
 
 class ActiveIngredientClass(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
 
-    active_ingredient = models.ManyToManyField('ActiveIngredient', blank=True, null=True)
+  active_ingredient = models.ManyToManyField('ActiveIngredient', blank=True, null=True)
 
-   def __unicode__(self):
-       return self.name
+  def __unicode__(self):
+    return self.name
 
 class ApplicationArea(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
 
-    #brand = models.ManyToManyField('Brand', blank=True, null=True)
+  #brand = models.ManyToManyField('Brand', blank=True, null=True)
 
-   def __unicode__(self):
-       return self.name
+  def __unicode__(self):
+    return self.name
 
 class Warning(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
-    image_url = models.TextField(blank=True)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
+  image_url = models.TextField(blank=True)
 
-   def __unicode__(self):
-       return self.name
+  def __unicode__(self):
+    return self.name
 
 class PestType(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
 
-   def __unicode__(self):
-       return self.name
+  def __unicode__(self):
+    return self.name
 
 class Category(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
-    image_url = models.TextField()
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
+  image_url = models.TextField()
 
-    sub_categories = models.ManyToManyField('SubCategory', null=True)
+  sub_categories = models.ManyToManyField('SubCategory', null=True)
 
-   def __unicode__(self):
-       return self.name
+  def __unicode__(self):
+    return self.name
 
 class SubCategory(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    row_entry_date = models.DateTimeField(blank=True, null=True)
-    row_update_date = models.DateTimeField(blank=True, null=True)
-    name = models.CharField(unique=True, max_length=50)
-    image_url = models.TextField(blank=True)
+  row_id = models.IntegerField(primary_key=True)
+  row_entry_date = models.DateTimeField(blank=True, null=True)
+  row_update_date = models.DateTimeField(blank=True, null=True)
+  name = models.CharField(unique=True, max_length=50)
+  image_url = models.TextField(blank=True)
 
-    #category = models.ForeignKey('Category', null=True)
-    pests = models.ManyToManyField('Pest', null=True)
+  #category = models.ForeignKey('Category', null=True)
+  pests = models.ManyToManyField('Pest', null=True)
 
-   def __unicode__(self):
-       return self.name
+  def __unicode__(self):
+    return self.name
 """
 class PestToPesticide(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    pest = models.ForeignKey(Pest, blank=True, null=True)
-    pesticide = models.ForeignKey('Pesticide', blank=True, null=True)
+  row_id = models.IntegerField(primary_key=True)
+  pest = models.ForeignKey(Pest, blank=True, null=True)
+  pesticide = models.ForeignKey('Pesticide', blank=True, null=True)
 
 class ApplicationAreaToBrand(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    application_area = models.ForeignKey(ApplicationArea, blank=True, null=True)
-    brand = models.ForeignKey('Brand', blank=True, null=True)
+  row_id = models.IntegerField(primary_key=True)
+  application_area = models.ForeignKey(ApplicationArea, blank=True, null=True)
+  brand = models.ForeignKey('Brand', blank=True, null=True)
 
 
 class BrandToPesticide(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    brand = models.ForeignKey(Brand, blank=True, null=True)
-    pesticide = models.ForeignKey('Pesticide', blank=True, null=True)
+  row_id = models.IntegerField(primary_key=True)
+  brand = models.ForeignKey(Brand, blank=True, null=True)
+  pesticide = models.ForeignKey('Pesticide', blank=True, null=True)
 
 
 class PestToSubCategory(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    pest = models.ForeignKey(Pest, blank=True, null=True)
-    sub_category = models.ForeignKey('SubCategory', blank=True, null=True)
+  row_id = models.IntegerField(primary_key=True)
+  pest = models.ForeignKey(Pest, blank=True, null=True)
+  sub_category = models.ForeignKey('SubCategory', blank=True, null=True)
 
 class PestTypeToPest(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    pest_type = models.ForeignKey(PestType, blank=True, null=True)
-    pest = models.ForeignKey(Pest, blank=True, null=True)
+  row_id = models.IntegerField(primary_key=True)
+  pest_type = models.ForeignKey(PestType, blank=True, null=True)
+  pest = models.ForeignKey(Pest, blank=True, null=True)
 
 
 class PesticideClassToPesticide(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    pesticide_class = models.ForeignKey(PesticideClass, blank=True, null=True)
-    pesticide = models.ForeignKey(Pesticide, blank=True, null=True)
+  row_id = models.IntegerField(primary_key=True)
+  pesticide_class = models.ForeignKey(PesticideClass, blank=True, null=True)
+  pesticide = models.ForeignKey(Pesticide, blank=True, null=True)
 
 
 class SubCategoryToCategory(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    category = models.ForeignKey(Category, blank=True, null=True)
-    sub_category = models.ForeignKey(SubCategory, blank=True, null=True)
+  row_id = models.IntegerField(primary_key=True)
+  category = models.ForeignKey(Category, blank=True, null=True)
+  sub_category = models.ForeignKey(SubCategory, blank=True, null=True)
 
 
 class WarningToPesticide(models.Model):
-    row_id = models.IntegerField(primary_key=True)
-    warning = models.ForeignKey(Warning, blank=True, null=True)
-    pesticide = models.ForeignKey(Pesticide)
+  row_id = models.IntegerField(primary_key=True)
+  warning = models.ForeignKey(Warning, blank=True, null=True)
+  pesticide = models.ForeignKey(Pesticide)
 """
