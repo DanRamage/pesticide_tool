17742ef9a2abaa9ee58fd52e2b833daff19c6e51
dramage <danramage@Dans-MacBook-Pro.local>
2014-06-16 09:57:55 -0400

    renamed setiings_local back.

--- pesticide_tool/settings.py
+++ pesticide_tool/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from os import path
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -78,5 +78,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-from settings_local-bluehost import *
+from settings_local import *
 
