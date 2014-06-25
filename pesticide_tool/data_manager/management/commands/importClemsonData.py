23c8a9deba2f4f199cdc012d701a56a118c83c69
dramage <danramage@Dans-MacBook-Pro.local>
2014-06-25 07:36:24 -0400

    Added flag to specify an active ingredient to start at to pul from clemson pesticide site.

--- pesticide_tool/data_manager/management/commands/importClemsonData.py
+++ pesticide_tool/data_manager/management/commands/importClemsonData.py
@@ -9,12 +9,17 @@ from _pesticideClemsonWebRequest import clemsonWebService
 logger = logging.getLogger('pesticide_tool')
 
 
-def queryClemsonWebService(config_file):
+def queryClemsonWebService(**kwargs):
   if logger:
     logger.info("Starting active ingredient lookups.")
-  webService = clemsonWebService(config_file, 'pesticide_tool')
+
+  webService = clemsonWebService(kwargs['config_file'], 'pesticide_tool')
+  if 'starting_ingredient' in kwargs:
+    query = ActiveIngredient.objects.filter(name__gte = kwargs['starting_ingredient']).all().order_by('name')
+  else:
+    query = ActiveIngredient.objects.all().order_by('name')
   #for layer in Layer.objects.all().order_by('name'):
-  for active_ingredient in ActiveIngredient.objects.all().order_by('name'):
+  for active_ingredient in query:
     if logger:
       logger.debug("%s retrieving info." % (active_ingredient.name))
     info = webService.searchByActiveIngredient(active_ingredient.name)
@@ -26,10 +31,11 @@ class Command(BaseCommand):
   option_list = BaseCommand.option_list  + (
       make_option("--ConfigFile", dest="config_file"),
       make_option("--ImportFromWeb", dest="import_from_web", action='store_true', default='false'),
+      make_option("--StartingActiveIngredient", dest="starting_ingredient", default=None),
   )
 
   def handle(self, *args, **options):
     if options['import_from_web']:
-      queryClemsonWebService(options['config_file'])
+      queryClemsonWebService(config_file = options['config_file'], starting_ingredient=options['starting_ingredient'])
 
     return
