1c791085935a9f9564b0830801f7447cf107f41e
dramage <danramage@Dans-MacBook-Pro.local>
2014-06-25 14:36:16 -0400

    Allow nulls in some Brad fields.

--- pesticide_tool/data_manager/models.py
+++ pesticide_tool/data_manager/models.py
@@ -43,9 +43,9 @@ class Brand(models.Model):
   label_url = models.TextField(blank=True)
 
   restricted_use = models.BooleanField(blank=True, null=True)
-  experimental_use = models.BooleanField(blank=True, null=True))
-  special_local_need = models.BooleanField(blank=True, null=True))
-  formulation = models.CharField(max_length=50, blank=True, null=True))
+  experimental_use = models.BooleanField(blank=True, null=True)
+  special_local_need = models.BooleanField(blank=True, null=True)
+  formulation = models.CharField(max_length=50, blank=True, null=True)
   epa_registration_number = models.CharField(unique=True, max_length=50)
 
   company_name = models.ManyToManyField('Company', blank=True)
