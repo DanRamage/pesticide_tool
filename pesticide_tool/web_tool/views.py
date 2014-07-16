eb819778efb21448f01a171234895b7f965eb4ee
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-16 14:22:52 -0400

    INitial checkin.

--- pesticide_tool/web_tool/views.py
+++ pesticide_tool/web_tool/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+#import warnings
 
+from django.template import RequestContext
+"""
+from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.db.models.base import ModelBase
+from django.db.models.manager import Manager
+from django.db.models.query import QuerySet
+from django.core import urlresolvers
+"""
 # Create your views here.
 def start_page(request, template='entry_page.html'):
     context = {'domain': get_domain(8000), 'domain8010': get_domain()}
