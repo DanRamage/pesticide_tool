340d6b32dcf95fc7538242cd33208087561ee3a5
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-16 14:05:25 -0400

    INitial checkin.

--- pesticide_tool/urls.py
+++ pesticide_tool/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^tool/', include('web_tool.urls')),
 )
 
 if settings.DEBUG:
