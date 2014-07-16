e072de0121471e36fad20c6622a713ab91fb1702
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-16 14:43:48 -0400

    INitial checkin.

--- pesticide_tool/web_tool/views.py
+++ pesticide_tool/web_tool/views.py
@@ -1,9 +1,8 @@
 #from django.shortcuts import render
 #import warnings
 
-from django.template import RequestContext
+#from django.template import RequestContext
 from django.shortcuts import render_to_response
-from utils import get_domain
 """
 from django.http import HttpResponse, Http404
 from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
