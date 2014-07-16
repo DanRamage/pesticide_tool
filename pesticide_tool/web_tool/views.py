7b9ef8842bd2a16589be09f239fdac81d5eff7ef
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-16 14:16:20 -0400

    INitial checkin.

--- pesticide_tool/web_tool/views.py
+++ pesticide_tool/web_tool/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def start_page(request, template='entry_page.html'):
+    context = {'domain': get_domain(8000), 'domain8010': get_domain()}
+    return render_to_response(template, RequestContext(request, context))
