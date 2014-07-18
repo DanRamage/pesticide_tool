7c932a983f0ea9c0e9facec27670213f18f80226
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-18 10:12:47 -0400

    INitial checkin

--- pesticide_tool/web_tool/views.py
+++ pesticide_tool/web_tool/views.py
@@ -3,6 +3,7 @@
 
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from data_manager.models import *
 """
 from django.http import HttpResponse, Http404
 from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
@@ -15,3 +16,8 @@ from django.core import urlresolvers
 def start_page(request, template='entry_page.html'):
     #context = {'domain': get_domain(8000), 'domain8010': get_domain()}
     return render_to_response(template, context_instance=RequestContext(request))
+
+def get_categories(request, template='pest_category.html'):
+  categories = Category.objects.all().order_by('name')
+  context = { 'categories': categories}
+  return render_to_response(template, context_instance=RequestContext(request, context))
