e9d2c02485d67ac72e34ccb9f4cc372b3712fcae
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-18 13:51:26 -0400

    INitial checkin

--- pesticide_tool/web_tool/urls.py
+++ pesticide_tool/web_tool/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from views import *
 
 urlpatterns = patterns('',
-    (r'^', start_page),
-    (r'category', pest_category),
+    (r'', start_page),
+    (r'^category', pest_category),
 )
