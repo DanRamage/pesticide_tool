98d4849d0516a3e77de0602bdfc74a60136cadbe
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-21 13:14:25 -0400

    INitial checkin

--- pesticide_tool/static/js/models.js
+++ pesticide_tool/static/js/models.js
@@ -121,8 +121,9 @@ function categoriesViewModel()
 {
   var self = this;
   self.showCategories = ko.observable(true);
+  self.showSubCategories = ko.observable(false);
   self.categoryModels = ko.observableArray([]); //The major categories of pests.
-
+  self.activeCategory = ko.observable();
   self.initialize = function()
   {
     var url = 'http://sccoastalpesticides.org/pesticide_tool/get_categories';
@@ -146,9 +147,14 @@ function categoriesViewModel()
     //Hide the categories button, then build the sub categories.
     self.showCategories(false);
     location.hash = category.href();
+    self.activeCategory(category);
+    self.showSubCategories(true);
     return;
   };
-
+  self.subCategoryClicked = function(category, event)
+  {
+    return;
+  }
 }
 
 function subCategoriesViewModel()
