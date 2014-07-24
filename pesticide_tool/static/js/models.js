43a04d01dbb186637affeaa4a56fc0257dca6e37
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-24 14:02:18 -0400

    ADded hashchange plugin.

--- pesticide_tool/static/js/models.js
+++ pesticide_tool/static/js/models.js
@@ -145,9 +145,9 @@ function categoriesViewModel()
   //Array to track which parts should be visible.
   self.visibleTracker = ko.observableArray([]);
   self.visibleList = self.visibleTracker.asDictionary('page');
-  self.visibleTracker.push({'page': 'category', 'visible': true});
-  self.visibleTracker.push({'page': 'sub_category', 'visible': false});
-  self.visibleTracker.push({'page': 'pest', 'visible': false});
+  self.visibleTracker.push({page: 'category', visible: true});
+  self.visibleTracker.push({page: 'sub_category', visible: false});
+  self.visibleTracker.push({page: 'pest', visible: false});
 
   self.categoryModels = ko.observableArray([]); //The major categories of pests.
   self.activeCategory = ko.observable(new categoryModel());
