f3cec974afc1b36fbe56c07cd7942e135cdcc972
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-25 08:00:57 -0400

    ADded hashchange plugin.

--- pesticide_tool/web_tool/views.py
+++ pesticide_tool/web_tool/views.py
@@ -48,7 +48,7 @@ def get_pests_for_subcategory(request, sub_category):
   if logger:
     logger.info("Begin get_pests_for_subcategory: %s" % (search_term))
 
-  sub_cat = SubCategory.objects.filter(name=sub_category)
+  sub_cat = SubCategory.objects.filter(name__icontains=sub_category)
   if logger:
     logger.debug(sub_cat)
   json = {
