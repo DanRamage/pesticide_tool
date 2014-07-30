e519783d0ba5f462d541ccfae23d9ce6f85ace54
dramage <danramage@Dans-MacBook-Pro.local>
2014-07-30 08:12:31 -0400

    get_ai_fo_pest defer loading of brands fields that aren't needed.

--- pesticide_tool/web_tool/views.py
+++ pesticide_tool/web_tool/views.py
@@ -62,7 +62,27 @@ def get_ai_for_pest(request, pest):
   if logger:
     logger.debug("Begin get_ai_for_pest: %s" % (search_term))
 
-  ai_list = ActiveIngredient.objects.filter(pests_treated__display_name__exact=search_term).order_by('cumulative_score')
+  ai_list = ActiveIngredient.objects.defer("brands__pests_treated").defer("brands__application_areas").filter(pests_treated__display_name__exact=search_term).order_by('cumulative_score')
+  ret_data = []
+  for ai in ai_list.all():
+    """
+    name = models.TextField(unique=True)
+    display_name = models.TextField(unique=False)
+    cumulative_score = models.FloatField(blank=True, null=True)
+    relative_potential_ecosystem_hazard = models.CharField(max_length=50, blank=True, null=True)
+
+    warnings = models.ManyToManyField('Warning', blank=True, null=True)
+    pests_treated = models.ManyToManyField('Pest')
+    pesticide_classes = models.ManyToManyField('PesticideClass')
+    brands = models.ManyToManyField('Brand')
+    ret_data.append({
+      'name': ai.name,
+      'display_name': ai.display_name,
+      'cumulative_score': ai.cumulative_score,
+      'relative_potential_ecosystem_hazard': ai.relative_potential_ecosystem_hazard,
+
+    })
+    """
   json = {
     "ai_list" : [ai.toDict for ai in ai_list.all()],
     "success": True
