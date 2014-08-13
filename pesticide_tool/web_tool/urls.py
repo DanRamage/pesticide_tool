81193bd18ad553ab864928f28a2601c8b7cb7461
DanRamage <dan@inlet.geol.sc.edu>
2014-08-13 10:50:02 -0400

    Added pesticide search page.

--- pesticide_tool/web_tool/urls.py
+++ pesticide_tool/web_tool/urls.py
@@ -4,6 +4,7 @@ from views import *
 urlpatterns = patterns('',
     (r'^category', pest_category),
     (r'^start', start_page),
+    (r'^pesticide_search', pesticide_search),
     (r'^pest_ai_page/(?P<pest_name>[\w-]*)', pest_ai_page),
     (r'^get_info_for_brand/(?P<brand>[\w-]*)', get_info_for_brand),
     (r'^get_categories', get_categories),
