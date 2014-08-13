64d257e9d7d8251425bc5abecece9d9dc8b1a490
DanRamage <dan@inlet.geol.sc.edu>
2014-08-13 12:42:25 -0400

    Added pesticide search page.

--- pesticide_tool/web_tool/views.py
+++ pesticide_tool/web_tool/views.py
@@ -26,7 +26,14 @@ def pest_category(request, template='pest_category.html'):
   return render_to_response(template, context_instance=RequestContext(request))
 
 def pesticide_search(request, template='pesticide_search.html'):
-  return render_to_response(template, context_instance=RequestContext(request))
+  active_ingredients = []
+  pesticides = Brand.object.all().only('name').order_by('name')
+
+  context = {
+    'pesticides': [rec.name for rec in pesticides],
+    'active_ingredients': active_ingredients
+  }
+  return render_to_response(template, context_instance=RequestContext(request, context))
 
 def pest_ai_page(request, pest_name, template='ais_for_pest.html'):
   search_term = pest_name
