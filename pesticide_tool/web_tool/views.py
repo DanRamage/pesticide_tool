da661ad76158717d84a04e16ab94393c19d74c55
DanRamage <dan@inlet.geol.sc.edu>
2014-08-15 11:01:32 -0400

    ADding ai page.

--- pesticide_tool/web_tool/views.py
+++ pesticide_tool/web_tool/views.py
@@ -49,10 +49,12 @@ def brand_page(request, brand_name, template='brand_page.html'):
     search_term = request.GET['brand_name']
   if logger:
     logger.debug("Begin brand_page: %s" % (search_term))
-  brand_json = {};
+  brand_json = [];
   try:
     brand_info = Brand.objects.filter(name__iexact=search_term).all()[:1].get()
-    brand_json = brand_info.toDict;
+    #Even though we are retreiving a single brand, the template is setup to handle
+    #multiple results. We put this result into an array for the template to use.
+    brand_json.append(brand_info.toDict)
   except Exception, e:
     if logger:
       logger.exception(e)
