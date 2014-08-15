67a5151fcb61c8d535eac75782b5b33c95efff13
DanRamage <dan@inlet.geol.sc.edu>
2014-08-15 15:05:56 -0400

    Sort brands in ai request.

--- pesticide_tool/static/js/models.js
+++ pesticide_tool/static/js/models.js
@@ -391,8 +391,7 @@ function pesticideSearchViewModel()
     //ADd the brands from the selected AI.
     if(ai.brands.length)
     {
-      var sorted_brands = ai.brands.sort();
-      self.activeBrands(sorted_brands);
+      self.activeBrands(ai.brands);
     }
     return(true);
   };
